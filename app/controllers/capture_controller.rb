d0dd1bd082de66525d6a69bb1ba32d9a24ae62f3
Tallak Tveide <tallakt@ubuntu-tt.(none)>
2010-03-06 14:21:37 +0100

    Added show single capture page and cookied for newest to less frequent rjs updates

--- app/controllers/capture_controller.rb
+++ app/controllers/capture_controller.rb
@@ -1,8 +1,19 @@
 class CaptureController < ApplicationController
   before_filter :update_feedback, :except => [:perform_capture]
 
+
+  def update_new_data
+    @new_data = true
+    if cookies[:newest]
+      cc = Capture.find_by_id @capture.id, :conditions => ['updated_at > ?', cookies[:newest]]
+      @new_data = false unless cc
+      cookies[:newest] = @capture.updated_at.to_s(:db)
+    end
+  end
+
   def index
     @capture = Capture.last_with_preview
+    update_new_data 
     respond_to do |format|
       format.html
       format.js
@@ -11,7 +22,15 @@ class CaptureController < ApplicationController
 
   def list
     @captures = Capture.paginate :page => params[:page], :order => 'created_at DESC', :per_page => 5
-    puts @captures.inspect
+    respond_to do |format|
+      format.html
+      format.js
+    end
+  end
+
+  def show
+    @capture = Capture.find params[:id]
+    update_new_data
     respond_to do |format|
       format.html
       format.js
